ef2399720c2711dc216f6002a22e28f546816efe
youssef <youssefzekry97@gmail.com>
2023-03-09 01:35:40 +0200

    Initialize OrderStatusDTO

---
+++ src/main/java/iti/jets/marketplace/dtos/OrderStatusDTO.java
@@ -0,0 +1,16 @@
+package iti.jets.marketplace.dtos;
+
+import java.util.Date;
+
+import iti.jets.marketplace.models.Product;
+import iti.jets.marketplace.models.User;
+import iti.jets.marketplace.models.UserorderproductId;
+
+public class OrderStatusDTO {
+	private UserorderproductId id;
+	private Product product;
+	private User user;
+	private String status;
+	private Integer productCount;
+	private Date orderDate;
+}
