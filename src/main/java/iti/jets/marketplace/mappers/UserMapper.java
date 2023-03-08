4835b4df7d6c8ea0d3c8166adfe7884bf3c0b87b
Hassan7740 <101173691+Hassan7740@users.noreply.github.com>
2023-03-08 22:37:15 +0200

    add git all users service and controllers

--- src/main/java/iti/jets/marketplace/mappers/UserMapper.java
+++ src/main/java/iti/jets/marketplace/mappers/UserMapper.java
@@ -6,9 +6,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
+import java.util.List;
+
 
 @Mapper(componentModel = "spring" , nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface UserMapper {
     UserDTO map(User user);
     User map(UserDTO userDTO);
+
+    List<UserDTO> map(List<User> allUsers);
 }
