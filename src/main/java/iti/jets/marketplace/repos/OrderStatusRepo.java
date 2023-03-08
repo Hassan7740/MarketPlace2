0a31d6013f83ab5a2b3e0c8555e0851e1fb5810c
youssef <youssefzekry97@gmail.com>
2023-03-09 01:38:36 +0200

    Add OrderStatusRepo

---
+++ src/main/java/iti/jets/marketplace/repos/OrderStatusRepo.java
@@ -0,0 +1,9 @@
+package iti.jets.marketplace.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import iti.jets.marketplace.models.Userorderproduct;
+
+public interface OrderStatusRepo extends JpaRepository<Userorderproduct,Integer> {
+	
+}
