2c358e7a4e87a1e4acaf4c479dceedee0a6e3913
youssef <youssefzekry97@gmail.com>
2023-03-09 01:38:07 +0200

    Add OrderStatusMapper

---
+++ src/main/java/iti/jets/marketplace/mappers/OrderStatusMapper.java
@@ -0,0 +1,16 @@
+package iti.jets.marketplace.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import iti.jets.marketplace.dtos.OrderStatusDTO;
+import iti.jets.marketplace.models.Userorderproduct;
+
+@Mapper(componentModel = "spring" , nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public interface OrderStatusMapper {
+
+	OrderStatusDTO userOrderProductToOrderStatusDTO(Userorderproduct userorderproduct);
+	
+	Userorderproduct orderStatusDtoToUserOrderProduct(OrderStatusDTO orderStatusDTO);
+}
