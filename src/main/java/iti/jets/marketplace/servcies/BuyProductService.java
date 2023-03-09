0e3c7114bfa2f4bf4ff6e588a2533d695e703faf
hamza <hamza1.alaa88@gmail.com>
2023-03-09 21:22:00 +0200

    dont comment my code

--- src/main/java/iti/jets/marketplace/servcies/BuyProductService.java
+++ src/main/java/iti/jets/marketplace/servcies/BuyProductService.java
@@ -1,35 +1,35 @@
-// package iti.jets.marketplace.servcies;
+package iti.jets.marketplace.servcies;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import iti.jets.marketplace.dtos.BuyProductDTO;
-// import iti.jets.marketplace.repos.ProductRepo;
-// import iti.jets.marketplace.utils.ResponseViewModel;
+import iti.jets.marketplace.dtos.BuyProductDTO;
+import iti.jets.marketplace.repos.ProductRepo;
+import iti.jets.marketplace.utils.ResponseViewModel;
 
-// @Service
-// public class BuyProductService {
-//     @Autowired
-//     ProductRepo productRepo;
+@Service
+public class BuyProductService {
+    @Autowired
+    ProductRepo productRepo;
 
-//     int productAmount;
+    int productAmount;
 
-//     public BuyProductService() {
-//     }
+    public BuyProductService() {
+    }
 
-//     public ResponseViewModel<Integer> getProdcutNumber(BuyProductDTO buyProductDTO) {
-//         Optional<Integer> prodctAmountOpt = productRepo.findNumberOfproduct(buyProductDTO.getId());
-//         if (!prodctAmountOpt.isPresent()) {
-//             return new ResponseViewModel<Integer>("product not found", 400, null);
-//         } else if (prodctAmountOpt.get() >= buyProductDTO.getAmount()) {
-//             productRepo.updateNumberOfProduct((prodctAmountOpt.get() - buyProductDTO.getAmount()),
-//                     buyProductDTO.getId());
-//             return new ResponseViewModel("the remain amount", 200, (prodctAmountOpt.get() - buyProductDTO.getAmount()));
-//         } else {
-//             return new ResponseViewModel<Integer>("amount not available", 200, -1);
-//         }
+    public ResponseViewModel<Integer> getProdcutNumber(BuyProductDTO buyProductDTO) {
+        Optional<Integer> prodctAmountOpt = productRepo.findNumberOfproduct(buyProductDTO.getId());
+        if (!prodctAmountOpt.isPresent()) {
+            return new ResponseViewModel<Integer>("product not found", 400, null);
+        } else if (prodctAmountOpt.get() >= buyProductDTO.getAmount()) {
+            productRepo.updateNumberOfProduct((prodctAmountOpt.get() - buyProductDTO.getAmount()),
+                    buyProductDTO.getId());
+            return new ResponseViewModel("the remain amount", 200, (prodctAmountOpt.get() - buyProductDTO.getAmount()));
+        } else {
+            return new ResponseViewModel<Integer>("amount not available", 200, -1);
+        }
 
-//     }
-// }
+    }
+}
